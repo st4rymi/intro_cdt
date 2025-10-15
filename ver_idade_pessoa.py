61cb767155aeda940bd0f0fa57860cb5587d2f1a
Rafaela Yumi <yumitagawa1701@gmail.com>
2025-10-15 02:43:40 -0300

    ver_idade_pessoa.py

--- ver_idade_pessoa.py
+++ ver_idade_pessoa.py
@@ -5,15 +5,15 @@ Saber a idade da pessoa;
 armazeanar a idade em uma variavel (int);
 caso a idade seja maior que 18 anos;
  exibir a mensagem "Você é maior de idade";
- Se não for maior que 18 anos, mostrar a mensagem 'voc~e é maior de idade'
+ Se não for maior que 18 anos, mostrar a mensagem 'Você é menor de idade'
 '''
 #Criando Estruturas
 print("\n¨¨¨¨¨¨¨¨¨¨¨¨¨¨")
 print("\fOlá, Seja bem-vindo! ^u^")
 print("\n¨¨¨¨¨¨¨¨¨¨¨¨¨¨")
 
-nome_pessoa = str(input(" Qual seu nome? "))
-idade_pessoa = int(input(" Qual é sua idade? "))
+nome_pessoa = str(input("Qual seu nome? "))
+idade_pessoa = int(input("Qual é sua idade? "))
 
 ## Lógica do programa 
 # se (if) variável (idade_pessoa) critério (>=) valor (18)
@@ -25,3 +25,4 @@ else:
     print ("Aah, que pena! Você é menor de idade. Tente na próxima! u_u")
 
 print(f"Olá, (nome-pessoa)! Sua idade é (idade-pessoa).")
+
