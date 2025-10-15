8e423364acca45e0655640b39e26ee0627aef576
Rafaela Yumi <yumitagawa1701@gmail.com>
2025-10-15 02:33:25 -0300

    Add files via upload

--- ver_idade_pessoa.py
+++ ver_idade_pessoa.py
@@ -1,30 +1,27 @@
 '''
 Primicias
-   Verificar a idade da pessoa.
-Saber a idade da pessoa;
-armazenar a idade em uma variavel (int);
+     Verificar a idade de uma pessoa.
+Saber a idade da pessoa; 
+armazeanar a idade em uma variavel (int);
 caso a idade seja maior que 18 anos;
-mostrar a mensagem "Você é maior de idade";
-se não for maior que 18 anos mostrar a mensagem "Você é menor de idade".;
-
+ exibir a mensagem "Você é maior de idade";
+ Se não for maior que 18 anos, mostrar a mensagem 'voc~e é maior de idade'
 '''
-# Criando Estruturas
-print("\n---------------------")
-print("\f Bem vindo Amigo(a)!!")
-print("\n---------------------")
+#Criando Estruturas
+print("\n¨¨¨¨¨¨¨¨¨¨¨¨¨¨")
+print("\fOlá, Seja bem-vindo! ^u^")
+print("\n¨¨¨¨¨¨¨¨¨¨¨¨¨¨")
 
-nome_pessoa = str(input("Qual o seu nome? "))
+nome_pessoa = str(input(" Qual seu nome? "))
+idade_pessoa = int(input(" Qual é sua idade? "))
 
-idade_pessoa = int(input("Qual é sua idade? "))
-
-# Lógica do prgrama
-
-# se (if) variavel (idade_pessoa) criterio (>=) valor (18)
-# condição (maior ou igual a 18)
+## Lógica do programa 
+# se (if) variável (idade_pessoa) critério (>=) valor (18)
+# condição (maior ou igual a 18 anos)
 if idade_pessoa >= 18:
-   print ("Muito bem, você é maior de idade! ^-^ ")
+    print("Muito bem! Você é maior de idade.^-^")
 
-   else:
-      print ("Aah, que pena! Você é menor de idade. u_u ")
+else:
+    print ("Aah, que pena! Você é menor de idade. Tente na próxima! u_u")
 
-      print(f"Olá, {nome_pessoa}, então sua idade é {idade_pessoa}")
+print(f"Olá, (nome-pessoa)! Sua idade é (idade-pessoa).")
